b3a819423be5f1de4ca3f087570af19a9bcb6725
manojkumarsingh123 <2015manoj11@gmail.com>
2024-10-22 15:13:24 +0530

    initial commit

---
+++ src-file/app.js
@@ -0,0 +1,19 @@
+const express =  require("express");
+
+const app =  express();
+
+//Request handler
+
+//when we hit hello in browser(making req) then server will response(Go to home page) 
+app.use("/hello",(req,res)=>{ 
+    res.send("Go to home page ")
+});
+
+app.use((req,res)=>{
+    res.send("when make a request then response ok ")
+});
+
+
+app.listen(3000,()=>{
+    console.log("server is started");
+});
