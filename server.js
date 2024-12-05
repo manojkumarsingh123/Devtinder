272b403c300140f403577a2ffb6406077c29c5f1
manojkumarsingh123 <2015manoj11@gmail.com>
2024-12-06 00:01:35 +0530

    database connection

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require("express");
-
+const connectDB = require("./src-file/config/database");
 const app = express();
 
 
-app.get("/getUserData", (req, res) => {
-  //try {
-  // Logic of DB call and get user data
-
-  throw new Error("dvbzhjf");
-  res.send("User Data Sent");
-  //   } catch (err) {
-  //     res.status(500).send("Some Error contact support team");
-  //   }
-});
-app.use("/", (err, req, res, next) => { // it will handle all unexpected errors
-  if (err) {
-    // Log your error
-    res.status(500).send("something went wrong");
-  }
-});
-
-app.listen(7777, () => {
-  console.log("Server is successfully listening on port 7777...");
-});
-
-//always use try and catch and at the end of your code use wild card error handling if 
-
-// try cath miss any error then wild card error mrthod will take care 
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 5000...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
---
+++ src-file/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://7974manojsingh:CVfiKYmhkCFGhlv2@namstenode.v7ksp.mongodb.net"
+  );
+};
+
+module.exports = connectDB;
