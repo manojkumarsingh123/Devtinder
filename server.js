b5f052c1fb073816595fb6a1dabe3a94f3ead05a
manojkumarsingh123 <2015manoj11@gmail.com>
2024-12-05 17:03:34 +0530

    middleware one route can have multiple route handler but accept only one response form single route

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const app = express();
 app.use('/user',(req,res,next)=>{
   res.send("hello from first route");
   console.log("From first route")
+  next()
 },
 
 (req,res,next)=>{
-  res.send("hello from second route");
   console.log("From second route")
+  res.send("hello from second route");
+  
 }
 )
 
-// here only get first output "hello from first route", beacuse there is no next and client will response only once
+// here only get first output "hello from first route", and response send to client but 
+//console.log("From second route")  this line also execute but when try to send res.send("hello from second route"); second 
+// resonse from the same route after send one response we will get an error Cannot set headers after they are sent to the client
+//Beacause we already sent one response not allow to send another response for the same route
 
 app.listen(3000);
