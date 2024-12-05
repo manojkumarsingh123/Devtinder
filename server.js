f65cee012a535680fd1b24dc650b71811f0f3105
manojkumarsingh123 <2015manoj11@gmail.com>
2024-12-06 00:18:24 +0530

    sending first data by post in db

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const connectDB = require("./src-file/config/database");
 const app = express();
+const User = require("./src-file/models/user");
 
+app.post("/signup", async (req, res) => {
+  // Creating a new instance of the User model
+  const user = new User({
+    firstName: "Sachin",
+    lastName: "Tendulkar",
+    emailId: "sachin@kohli.com",
+    password: "sachin@123",
+  });
+
+  try {
+    await user.save(); // saving the data in database
+    res.send("User Added successfully!"); // after saving the data sending back to the response 
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
     app.listen(3000, () => {
-      console.log("Server is successfully listening on port 5000...");
+      console.log("Server is successfully listening on port 7777...");
     });
   })
   .catch((err) => {
--- src-file/config/database.js
+++ src-file/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://7974manojsingh:CVfiKYmhkCFGhlv2@namstenode.v7ksp.mongodb.net"
+    "mongodb+srv://7974manojsingh:CVfiKYmhkCFGhlv2@namstenode.v7ksp.mongodb.net/devtinder"
   );
 };
 
